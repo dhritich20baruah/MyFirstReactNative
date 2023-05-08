ae8b2bdab5ef64d652bb09077c1a8f4c6ac1311a
Dhriti <dhriti.baruah20@gmail.com>
2023-05-08 21:32:56 +0530

    Text Input

--- App.js
+++ App.js
@@ -5,27 +5,19 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 // import type {PropsWithChildren} from 'react';
-import exStyles from './style'
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-} from 'react-native';
+import exStyles from './style';
+import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('');
   return (
     <View style={{backgroundColor: 'cyan'}}>
       <Text style={exStyles.textBox}>Hello React Native</Text>
       <Text style={styles.textBox}>Hello my friend we meet again</Text>
-      <Text style={[styles.textBox, exStyles.bg, {margin: 2}]}>Hello my friend we meet again</Text>
-
+      <Text style={[styles.textBox, exStyles.bg, {margin: 2}]}>Your Name is: {name}</Text>
+      <TextInput placeholder="Enter your name" style={styles.inputBox} value={name} onChangeText={(text)=>setName(text)}/>
       <Button title="Press Here"></Button>
     </View>
   );
@@ -37,6 +29,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
   },
+  inputBox: {
+    padding: 2,
+    margin: 2,
+    backgroundColor: 'white',
+    border: 'none',
+    fontSize: 20,
+  }
 });
 
 export default App;
